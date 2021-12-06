96bb5e1657a06ba6f6cc971f9e2f06eaf18a39b6
amirahmedimtiaz <64948625+amirahmedimtiaz@users.noreply.github.com>
2021-12-06 12:56:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -73,7 +73,7 @@ class App extends Component{
 
 
   calculateFacelocation = (data) => {
-    //console.log(data);
+    
     const clarifaiFace =  data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById('inputImage');
     const width = Number(image.width);
